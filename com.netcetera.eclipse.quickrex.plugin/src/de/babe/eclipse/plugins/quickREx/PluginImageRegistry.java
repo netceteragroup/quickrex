7b2a741dc894e04edc016dd0c9d20374d70d4409
Philippe Marschall <philippe.marschall@gmail.com>
2014-07-07 16:36:57 +0000

    - clean up

--- com.netcetera.eclipse.quickrex.plugin/src/de/babe/eclipse/plugins/quickREx/PluginImageRegistry.java
+++ com.netcetera.eclipse.quickrex.plugin/src/de/babe/eclipse/plugins/quickREx/PluginImageRegistry.java
@@ -10,20 +10,14 @@
  *******************************************************************************/
 package de.babe.eclipse.plugins.quickREx;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-
-import org.eclipse.core.runtime.FileLocator;
-import org.eclipse.core.runtime.Path;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.resource.ImageRegistry;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
 
 /**
  * @author bastian.bergerhoff, georg.sendt
  */
 public class PluginImageRegistry extends ImageRegistry {
-
-  private static URL iconBaseURL = null;
 
   public static final String IMG_JCOPY = "IMG_JCOPY"; //$NON-NLS-1$
 
@@ -33,14 +27,8 @@ public class PluginImageRegistry extends ImageRegistry {
    * @param p_plugin
    */
   public PluginImageRegistry(QuickRExPlugin p_plugin) {
-    super();
-    try {
-      if (iconBaseURL == null) {
-        iconBaseURL = FileLocator.find(p_plugin.getBundle(), new Path("/icons/"), null); //$NON-NLS-1$
-      }
-      put(IMG_JCOPY, ImageDescriptor.createFromURL(new URL(iconBaseURL, "jcopy.gif"))); //$NON-NLS-1$
-    } catch (MalformedURLException e) {
-      throw new RuntimeException(e);
-    }
+    String pluginId = p_plugin.getBundle().getSymbolicName();
+    ImageDescriptor descriptor = AbstractUIPlugin.imageDescriptorFromPlugin(pluginId, "icons/jcopy.gif");  //$NON-NLS-1$
+    put(IMG_JCOPY, descriptor);
   }
 }
