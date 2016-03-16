ccfb6a111700e9fe32c836c2d5b4337d90244189
Philippe Marschall <philippe.marschall@netcetera.com>
2016-03-16 09:46:20 +0100

    - Javadoc

--- com.netcetera.eclipse.quickrex.plugin/src/de/babe/eclipse/plugins/quickREx/regexp/jdk/CancellableCharSequence.java
+++ com.netcetera.eclipse.quickrex.plugin/src/de/babe/eclipse/plugins/quickREx/regexp/jdk/CancellableCharSequence.java
@@ -2,6 +2,10 @@ package de.babe.eclipse.plugins.quickREx.regexp.jdk;
 
 import java.util.concurrent.CancellationException;
 
+/**
+ * A {@link Character} that throws {@link CancellationException}
+ * after {@link #cancel()} has been called.
+ */
 public final class CancellableCharSequence implements CharSequence {
 
   private CharSequence delegate;
@@ -12,10 +16,20 @@ public final class CancellableCharSequence implements CharSequence {
     this.canceled = false;
   }
 
+  /**
+   * Factory method.
+   *
+   * @param charSequence the char sequence to wrap
+   * @return the wrapped char sequence
+   */
   public static CancellableCharSequence wrap(CharSequence charSequence) {
     return new CancellableCharSequence(charSequence);
   }
 
+  /**
+   * Marks this object as cancelled. After this all interface methods throw
+   * a {@link CancellationException}.
+   */
   public void cancel() {
     this.canceled = true;
   }
@@ -36,6 +50,12 @@ public final class CancellableCharSequence implements CharSequence {
   public CharSequence subSequence(int start, int end) {
     assertNotCanceled();
     return delegate.subSequence(start, end);
+  }
+
+  @Override
+  public String toString() {
+    assertNotCanceled();
+    return delegate.toString();
   }
 
   private void assertNotCanceled() {
